ba10810784542ebb6e59db9aa598bbf3fe9e0c0e
heymeck <hemantgupta1408@gmail.com>
2024-04-10 18:28:40 +0530

    learned about eval function js

--- Javascript/59 Exercise Faulty calculator/exercise.js
+++ Javascript/59 Exercise Faulty calculator/exercise.js
@@ -1,4 +1,22 @@
+let a= Math.random();
+console.log(a);
 console.log("hello world");
 console.log("This is your calculator");
-let n1=prompt("Enter your no 1");
+let n1=prompt("Enter your first no ");
 console.log(n1);
+let op = prompt("Enter Your operation to be performed ");
+let n2 = prompt("Enter your second no");
+let obj={
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+if (a>.1) {
+    alert(`The result is ${eval(`${n1} ${op} ${n2}`)}`);
+}else{
+    // perform wrong calculation
+    op=obj[op];
+    alert(`The result is ${eval(`${n1} ${op} ${n2}`)}`);
+}
+
