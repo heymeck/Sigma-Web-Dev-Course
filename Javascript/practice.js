afe898ca4b38af8d92e003ad4941f8c7226a23c0
heymeck <hemantgupta1408@gmail.com>
2024-04-15 18:26:45 +0530

    created factorial finding in practice.js using reduce function

--- Javascript/practice.js
+++ Javascript/practice.js
@@ -11,4 +11,21 @@ console.log(b);
 var b=45;
 console.log(b);
 
+// factorial finder using reduce
+let fac=7;
+arr=[];
+if(fac==0 || fac==1){
+    // console.log("1");
+    return 0;
+}
+else{
+for (let index = 1; index <= fac; index++) {
+    arr.push(index);
+}
+console.log(arr);
+factorial=(a,b) =>{
+    return a*b;
+}}
+console.log(arr.reduce(factorial));
+
 
